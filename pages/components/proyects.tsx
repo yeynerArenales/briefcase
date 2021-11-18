ddb6fc4ec662e51b96e5b16f153429e24e88a374
Yeyner Arenales <yeyner.arenales0811@gmail.com>
2021-11-18 11:16:57 -0400

    [Development] add proyect

---
+++ pages/components/proyects.tsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import styles from '../../styles/proyects.module.css';
+
+const Proyects = () => {
+    return (
+      <React.Fragment>
+        <div>
+          <h2 className={styles.title}>
+                Proyectos
+          </h2>
+        </div>
+      </React.Fragment>
+    )
+}
+
+export default Proyects;
