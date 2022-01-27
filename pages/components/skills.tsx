a7e28c4fd45f57730fc4e6af72e0251c4936ec60
Yeyner Arenales <yeyner.arenales0811@gmail.com>
2022-01-27 11:52:38 -0400

    [DEVELOPMENT] add proyect and update npm

--- pages/components/skills.tsx
+++ pages/components/skills.tsx
@@ -1,113 +1,33 @@
 import React from 'react'
 import Image from 'next/image'
 import styles from '../../styles/skills.module.css'
-import javascriptImage from '../../public/javascript.png'
-import typescriptImage from '../../public/typescript_2.png'
-import nodeImage from '../../public/nodejs.png'
-import cImage from '../../public/c.png'
-import reactImage from '../../public/react.png'
-import angularImage from '../../public/angular.png'
-import expressImage from '../../public/express.png'
-import netImage from '../../public/net.png'
-import nextImage from '../../public/next.png'
-
-import mongoImage from '../../public/mongodb.png'
-import mySqlImage from '../../public/mysql.png'
-import postgreImage from '../../public/postgre.png'
+import { skills } from '../../public/skills'
 
 const Skills = () => {
     return (
-      <React.Fragment>
-        <div id="skills" className={styles.container}>
-            <h2 className={styles.title}>Skills</h2>
-            <div className={styles.skills}>
-                <div className={styles.containerImage}>
-                    <Image
-                    src={javascriptImage}
-                    alt="Javascript Image"
-                    className={styles.image}
-                    />
-                </div>
-                <div className={styles.containerImage}>
-                    <Image
-                    src={nodeImage}
-                    alt="Node Js Image"
-                    className={styles.image}
-                    />
-                </div>
-                <div className={styles.containerImage}>
-                    <Image
-                    src={cImage}
-                    alt="C# Image"
-                    className={styles.image}
-                    />
-                </div>
-                <div className={styles.containerImage}>
-                    <Image
-                    src={typescriptImage}
-                    alt="TypeScript Image"
-                    className={styles.image}
-                    />
-                </div>
-                <div className={styles.containerImage}>
-                    <Image
-                    src={reactImage}
-                    alt="React Image"
-                    className={styles.image}
-                    />
-                </div>
-                {/* <div className={styles.containerImage}>
-                    <Image
-                    src={nextImage}
-                    alt="Next Image"
-                    className={styles.image}
-                    />
-                </div>
-                <div className={styles.containerImage}>
-                    <Image
-                    src={netImage}
-                    alt=".Net Image"
-                    className={styles.image}
-                    />
-                </div> */}
-                <div className={styles.containerImage}>
-                    <Image
-                    src={expressImage}
-                    alt="Express Image"
-                    className={styles.image}
-                    />
-                </div>
-                <div className={styles.containerImage}>
-                    <Image
-                    src={angularImage}
-                    alt="Angular Image"
-                    className={styles.image}
-                    />
-                </div>
-                <div className={styles.containerImage}>
-                    <Image
-                    src={mongoImage}
-                    alt="Mongo Image"
-                    className={styles.image}
-                    />
-                </div>
-                <div className={styles.containerImage}>
-                <Image
-                src={postgreImage}
-                alt="Postgre Image"
-                className={styles.image}
-                />
-                </div>
-                <div className={styles.containerImage}>
-                <Image
-                src={mySqlImage}
-                alt="MySql Image"
-                className={styles.image}
-                />
+        <React.Fragment>
+            <div id="skills" className={styles.container}>
+                <h2 className={styles.title}>Skills</h2>
+                <div className={styles.skills}>
+                    {
+                        skills.map(skill => {
+                            return (
+                                <div key={skill.name} className={styles.containerImage}>
+                                    <Image
+                                        src={skill.imgUrl}
+                                        alt={skill.name}
+                                        width='90px'
+                                        height='90px'
+                                        className={styles.image}
+                                    />
+                                </div>
+                            )
+                        })
+                    }
+
                 </div>
             </div>
-        </div>
-      </React.Fragment>
+        </React.Fragment >
     )
 }
 
---
+++ public/skills.ts
@@ -0,0 +1,48 @@
+
+type Skill = {
+    imgUrl: string,
+    name: string
+}
+
+export const skills: Skill[] = [
+    {
+        name: 'Javascript',
+        imgUrl: '/javascript.png'
+    },
+    {
+        name: 'NodeJs',
+        imgUrl: '/nodejs.png'
+    },
+    {
+        name: 'C#',
+        imgUrl: '/c.png'
+    },
+    {
+        name: 'ReactJS',
+        imgUrl: '/react.png'
+    },
+    {
+        name: 'Express',
+        imgUrl: '/express.png'
+    },
+    {
+        name: 'AngularJS',
+        imgUrl: '/angular.png'
+    },
+    {
+        name: 'Mongo',
+        imgUrl: '/mongodb.png'
+    },
+    {
+        name: 'MySql',
+        imgUrl: '/mysql.png'
+    },
+    {
+        name: 'Postgre',
+        imgUrl: '/postgre.png'
+    },
+    {
+        name: "TypeScript",
+        imgUrl: "/typescript_2.png"
+    }
+]
